e34c68afac7c50ef9b99324e2835aa2d8ddbc6b2
CivilBot <civilbots@gmail.com>
2019-03-11 20:26:18 +0530

    supplied ingredients thru state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,28 @@ import React from 'react';
 import BurgerIngredient from "./BurgerIngredients/BurgerIngredient";
 import classes from './Burger.module.css';
 
-const burger = props =>
+const burger = props => {
+    console.log(`${JSON.stringify(props.ingredients)}`);
+    const ingKeys = Object.keys(props.ingredients)
+        .map(ingKey => {
+            return [...Array(props.ingredients[ingKey])]
+                .map((_, i) => {
+                    return <BurgerIngredient key={ingKey + i} type={ingKey}/>
+                });
+        });
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type='bread-top'/>
+            {ingKeys}
+            <BurgerIngredient type='bread-bottom'/>
+        </div>);
+};
+/*
     <div className={classes.Burger}>
         <BurgerIngredient type='bread-top'/>
         <BurgerIngredient type='cheese'/>
         <BurgerIngredient type='meat'/>
         <BurgerIngredient type='bread-bottom'/>
     </div>;
-
+*/
 export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,14 +2,24 @@ import React, {Component} from 'react';
 import Hoc from '../../hoc/hoc';
 import Burger from '../../components/Burger/Burger';
 
-class BurgerBuilder extends Component{
-    render(){
-        return(
+class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 2,
+            bacon: 1,
+            meat:1,
+            cheese: 2
+        }
+    };
+
+    render() {
+        return (
             <Hoc>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Hoc>
         );
     }
 }
+
 export default BurgerBuilder;
