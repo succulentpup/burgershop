64f3c242e7fe7d6a56598aa4c6370b4e573ca77a
CivilBot <civilbots@gmail.com>
2019-03-11 21:12:19 +0530

    no ingredients case is taken care

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,14 @@ import classes from './Burger.module.css';
 
 const burger = props => {
     console.log(`${JSON.stringify(props.ingredients)}`);
-    const ingKeys = Object.keys(props.ingredients)
+    let ingKeys = Object.keys(props.ingredients)
         .map(ingKey => {
             return [...Array(props.ingredients[ingKey])]
                 .map((_, i) => {
                     return <BurgerIngredient key={ingKey + i} type={ingKey}/>
                 });
-        });
+        }).reduce((acc,cur) => acc.concat(cur), []);
+    if (ingKeys.length === 0) ingKeys = <p>Start adding ingredients</p>;
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top'/>
@@ -18,12 +19,4 @@ const burger = props => {
             <BurgerIngredient type='bread-bottom'/>
         </div>);
 };
-/*
-    <div className={classes.Burger}>
-        <BurgerIngredient type='bread-top'/>
-        <BurgerIngredient type='cheese'/>
-        <BurgerIngredient type='meat'/>
-        <BurgerIngredient type='bread-bottom'/>
-    </div>;
-*/
 export default burger;
