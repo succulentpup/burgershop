59df6c50a7b64757d4b609a9fbd3a132ded1e4bb
Ganesh Adapa <gadapa@berkadia.com>
2019-03-14 16:13:27 +0530

    Added order form summary
    Pending work is to correctly place the orderform summary on UI.
    Also, show it on demand.

--- src/components/Burger/BuildControls/BurgerControls.js
+++ src/components/Burger/BuildControls/BurgerControls.js
@@ -20,7 +20,7 @@ const BurgerControls = props => (
                 disable={props.disable[ctrl.type]}
             />
         ))}
-        <button className={classes.OrderButton} disabled={!props.purchasable}>ORDER</button>
+        <button className={classes.OrderButton} disabled={!props.purchasable}>ORDER NOW</button>
     </div>
 );
 
